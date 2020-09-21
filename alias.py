cf90d3d4306519437f3ef3c398d65f3174576b55
MitsukiUsui <mitsuo.com.mu@gmail.com>
2020-09-21 23:25:31 +0900

    POL-83: fix alias.py

--- alias.py
+++ alias.py
@@ -7,16 +7,16 @@ from politylink.helpers import BillFinder
 def main(fp):
     bill_finder = BillFinder()
     client = GraphQLClient()
-    df = pd.read_csv(fp)
-    for bill_text, bill_alias in zip(df['bill_text'], df['bill_alias']):
-        bills = bill_finder.find(bill_text)
-        if len(bills) == 1:
-            bill = bills[0]
-            bill.alias = bill_alias
-            client.exec_merge_bill(bill)
-            print(f'set alias={bill_alias} for {bill.bill_number}')
-        else:
-            print(f'found {len(bills)} Bills for {bill_text}')
+    df = pd.read_csv(fp, names=['text', 'aliases'])
+    for text, aliases in zip(df['text'], df['aliases']):
+        bills = bill_finder.find(text)
+        if len(bills) != 1:
+            print(f'found {len(bills)} Bills for {text}')
+            continue
+        bill = bills[0]
+        bill.aliases = aliases.split(';')
+        client.exec_merge_bill(bill)
+        print(f'set alias={bill.aliases} for {bill.bill_number}')
 
 
 if __name__ == '__main__':
