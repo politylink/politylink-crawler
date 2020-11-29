8d7bec189cb0e7380c7b7499782458ca6b7695be
MitsukiUsui <mitsuo.com.mu@gmail.com>
2020-11-29 22:33:48 +0900

    update data

--- crawler/spiders/cas_spider.py
+++ crawler/spiders/cas_spider.py
@@ -5,7 +5,7 @@ from crawler.utils import UrlTitle
 class CasSpider(TableSpiderTemplate, ManualSpiderTemplate):
     name = 'cas'  # 内閣官房
     domain = 'cas.go.jp'
-    start_urls = ['http://www.cas.go.jp/jp/houan/201.html']
+    start_urls = ['http://www.cas.go.jp/jp/houan/203.html']
 
     table_idx = 1
     bill_col = 0
--- crawler/spiders/mod_spider.py
+++ crawler/spiders/mod_spider.py
@@ -14,4 +14,10 @@ class ModSpider(ManualSpiderTemplate):
         {'title': UrlTitle.SINKYU_PDF,
          'bill': '防衛省設置法の一部を改正する法律案',
          'url': 'https://www.mod.go.jp/j/presiding/houan/pdf/201_200131/04.pdf'},
+        {'title': UrlTitle.GAIYOU_PDF,
+         'bill': '第203回国会閣法第7号',
+         'url': 'https://www.mod.go.jp/j/presiding/houan/pdf/203_201106/01.pdf'},
+        {'title': UrlTitle.SINKYU_PDF,
+         'bill': '第203回国会閣法第7号',
+         'url': 'https://www.mod.go.jp/j/presiding/houan/pdf/203_201106/04.pdf'},
     ]
