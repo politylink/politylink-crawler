ccc7cf9d518f506d67553256300810af7753ee53
MitsukiUsui <mitsuo.com.mu@gmail.com>
2020-12-26 09:04:15 +0900

    POL-175: fix http cache expiration

--- crawler/settings.py
+++ crawler/settings.py
@@ -82,7 +82,7 @@ DOWNLOAD_DELAY = 1
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 60 * 10 * 1000
+HTTPCACHE_EXPIRATION_SECS = 60 * 10
 HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
