48c398db6cea00340e989cfee1946a13bb98f5b5
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-04-14 22:31:01 +0900

    POL-283: store MinutesText.date

--- crawler/spiders/minutes_spider.py
+++ crawler/spiders/minutes_spider.py
@@ -8,7 +8,6 @@ import scrapy
 from crawler.spiders import SpiderTemplate
 from crawler.utils import build_minutes, build_speech, extract_topics, build_url, UrlTitle, build_minutes_activity, \
     clean_speech
-from politylink.elasticsearch.client import ElasticsearchClient
 from politylink.elasticsearch.schema import MinutesText
 from politylink.nlp.keyphrase import KeyPhraseExtractor
 
@@ -19,8 +18,7 @@ class MinutesSpider(SpiderTemplate):
     name = 'minutes'
     domain = 'ndl.go.jp'
 
-    def __init__(self, start_date, end_date, speech='false', text='false', overwrite='false',
-                 elasticsearch='http://localhost:9200', *args, **kwargs):
+    def __init__(self, start_date, end_date, speech='false', text='false', overwrite='false', *args, **kwargs):
         super(MinutesSpider, self).__init__(*args, **kwargs)
         self.start_date = start_date
         self.end_date = end_date
@@ -30,7 +28,6 @@ class MinutesSpider(SpiderTemplate):
         self.next_pos = 1
         self.num_key_phrases = 3
         self.key_phrase_extractor = KeyPhraseExtractor()
-        self.es_client = ElasticsearchClient(url=elasticsearch)
 
     def build_next_url(self):
         return 'https://kokkai.ndl.go.jp/api/meeting?from={0}&until={1}&startRecord={2}&maximumRecords=5&recordPacking=JSON'.format(
@@ -131,7 +128,8 @@ class MinutesSpider(SpiderTemplate):
             minutes_text_lst.append(MinutesText({
                 'id': minutes.id,
                 'title': minutes.name,
-                'body': full_text
+                'body': full_text,
+                'date': meeting_rec['date']
             }))
 
         return minutes_lst, minutes_text_lst, activity_lst, speech_lst, url_lst
