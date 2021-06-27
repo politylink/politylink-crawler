a58b998f61d431447b155cd75bf14dfc428fb92f
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-06-28 02:07:55 +0900

    POL-298: WIP fix test case & refactor

--- crawler/utils/common.py
+++ crawler/utils/common.py
@@ -168,138 +168,6 @@ def to_date_str(dt):
     return '{:02d}-{:02d}-{:02d}'.format(dt.year, dt.month, dt.day)
 
 
-def extract_minutes_schedule(first_speech, bullets=None, blocks=None):
-    """
-    会議録の最初の発言（=会議録情報）からその会議に付された案件のリストを抽出する
-    a.k.a format_first_speech
-
-    :param first_speech: 会議録の最初の発言
-    :param bullets: 議題の先頭文字の集合
-    :param blocks: セクション区切り文字の集合
-
-    :return: 会議に付された案件のリスト
-    """
-
-    bullets = bullets or set()
-    blocks = blocks or set()
-
-    is_schedule = False
-    schedule_lines = list()
-    buffer = ''
-    for line in first_speech.split('\n'):
-        line = line.strip()
-        if contains_word(line, ['本日の会議に付した案件', '本日の公聴会で意見を聞いた案件']):
-            is_schedule = True
-            schedule_lines = list()  # 複数回ヒットする場合は最後を優先する
-            continue
-        if line and is_schedule:
-            if line[0] in blocks:
-                is_schedule = False
-                continue
-
-            if bullets:  # トピック内の改行が存在する可能性あり
-                if line[0] in bullets:
-                    if buffer:
-                        schedule_lines.append(buffer)
-                    buffer = line[1:]
-                else:
-                    buffer += line
-            else:  # トピック内の改行は存在しない
-                schedule_lines.append(line)
-    if buffer:
-        schedule_lines.append(buffer)
-    return schedule_lines
-
-
-def extract_topics_from_line(line):
-    line = re.sub(r'^第?(一|二|三|四|五|六|七|八|九|十)+(　|、)?', '', line)
-    line = line.replace('、', '\u3000')  # 読点はトピック名の区切りとして\u3000に置換
-    line = line.replace('）及び', '）\u3000')  # "）及び"はトピック名の区切りとして"及び"を削除し\u3000に置換
-
-    topic_words = ['法律案', '法案', '決議案', '議決案', '調査', '調書', '協定', '承認', '予算', '互選', '件', '決算書', '計算書', '請願', '質疑']
-    topics = []
-    for maybe_topic in line.split():
-        if contains_word(maybe_topic, topic_words):
-            topics.append(maybe_topic)
-    return topics
-
-
-def extract_topics_v2(first_speech, house):
-    if house == '衆議院':
-        minutes_schedule = extract_minutes_schedule(first_speech, bullets={}, blocks={'―', '◇'})
-    elif house == '参議院':
-        minutes_schedule = extract_minutes_schedule(first_speech, bullets={'○'}, blocks={'─'})
-    else:
-        raise ValueError(f'unknown house: {house}')
-
-    topics = []
-    for line in minutes_schedule:
-        topics += extract_topics_from_line(line)
-    return topics
-
-
-def extract_topics(first_speech):
-    def format_first_speech(first_speech):
-        start_idx, end_idx = re.search(r'○?本日の会議に付した案件|○?本日の公聴会で意見を聞いた案件', first_speech).span()
-        speech_lst = []
-
-        # 議事日程を取得
-        # 議題が記載されている以前の文章を除くためのindex
-        schedule_idx = re.search(r'議事日程', first_speech)
-        if schedule_idx is not None:
-            speech_lst.append(first_speech[schedule_idx.end():start_idx])
-
-        # 明示的に記載されて案件を取得
-        speech_lst.append(first_speech[end_idx:])  # end_idxより後の文章を取得
-
-        # 改行を削除
-        format_speech = ''
-        for speech in speech_lst:
-            if re.search(r'\r\n○', speech) is not None:
-                speech = speech.replace('\r\n\u3000', '')  # トピック名の途中の\r\n\u3000を削除
-                speech = speech.replace('\r\n○', '\u3000')  # \r\n○はトピック名の区切りとして\u3000に置換
-            else:
-                speech = speech.replace('\r\n\u3000\u3000', '')  # \r\n○がない場合は、\r\n\u3000はトピック名の区切りなので削除しない
-
-            speech = speech.replace('、', '\u3000')  # 読点はトピック名の区切りとして\u3000に置換
-            speech = speech.replace('）及び', '）\u3000')  # "）及び"はトピック名の区切りとして"及び"を削除し\u3000に置換
-
-            format_speech += speech
-        return format_speech
-
-    topics = []
-    try:
-        first_speech = format_first_speech(first_speech)
-        topic_pattern = re.compile(r'\w+\S+(法律案|法案|決議案|議決案|調査|使用(総)?調書|特別措置法案|予算|互選|件|決算書|計算書|請願|質疑)(（.+?）)?')
-        for m in topic_pattern.finditer(first_speech):
-            topic = m.group()
-            topic = re.sub(r'^第?(一|二|三|四|五|六|七|八|九|十)+(　|、)?', '', topic)
-            # remove start and end spaces
-            topic = topic.strip()
-            if topic not in topics:
-                topics.append(topic)
-    except Exception:
-        LOGGER.exception(f'failed to parse topics from {first_speech}')
-    return topics
-
-
-def clean_topic(topic):
-    topic = topic.strip()
-    if topic.endswith('ため'):
-        return topic[:-2]
-    return topic
-
-
-def extract_topic_ids(speech, bill_id2names):
-    topic_ids = []
-    for bill_id, bill_name in bill_id2names.items():
-        if bill_name in speech:
-            topic_ids.append(bill_id)
-    if len(topic_ids) > 1:
-        LOGGER.debug(f'found multiple topics: speech={speech}, topic_ids={topic_ids}')
-    return topic_ids
-
-
 def contains_word(text, allow_list=None, block_list=None):
     allow_list = allow_list or list()
     block_list = block_list or list()
@@ -427,3 +295,10 @@ def build_bill_text(bill_id, texts):
     bill_text.supplement = ''.join(texts[supplement_idx + 1:reason_idx])
     bill_text.reason = ''.join(texts[reason_idx + 1:])
     return bill_text
+
+
+def get_offset(s):
+    for i, c in enumerate(s):
+        if c not in {' ', '　'}:
+            return i
+    return -1
---
+++ tests/utils/test_common.py
@@ -0,0 +1,64 @@
+import pytest
+
+from crawler.utils import parse_name_str, extract_bill_number_or_none, deduplicate, clean_speech, \
+    extract_bill_action_types, build_bill_text, get_offset
+from politylink.graphql.schema import BillActionType
+
+
+def test_parse_name_str():
+    assert ('一郎', '逢沢', 'いちろう', 'あいさわ') == parse_name_str('逢沢　一郎（あいさわ　いちろう）')
+    assert ('蓮舫', '', 'れんほう', '') == parse_name_str('蓮舫（れんほう）')
+
+
+def test_extract_bill_number_or_none():
+    assert '第204回国会閣法第9号' == extract_bill_number_or_none('地方税法等の一部を改正する法律案（204国会閣9）')
+    assert '第204回国会閣法第9号' == extract_bill_number_or_none('第204回国会閣法第9号')
+    assert '第200回国会衆法第1号' == extract_bill_number_or_none('200衆1')
+    assert extract_bill_number_or_none('地方税法等の一部を改正する法律案') is None
+
+
+def test_deduplicate():
+    assert [] == deduplicate([])
+    assert ['aaa', 'bbb', 'ccc', 'ddd'] == deduplicate(['aaa', 'bbb', 'ccc', 'aaa', 'ddd', 'bbb'])
+
+
+def test_clean_speech():
+    before = '○議長（大島理森君）　各請願は委員長の報告を省略して採択するに御異議ありませんか。 　　　　〔「異議なし」と呼ぶ者あり〕'
+    after = '各請願は委員長の報告を省略して採択するに御異議ありませんか。〔「異議なし」と呼ぶ者あり〕'
+    assert after == clean_speech(before)
+
+
+def test_extract_bill_action_types():
+    assert [BillActionType.QUESTION] == extract_bill_action_types('これより質疑に入ります。')
+    assert [] == extract_bill_action_types('本案の趣旨の説明につきましては、これを省略します')
+
+
+def test_build_bill_text():
+    texts = [
+        '犬法の一部を次のように改正する。',
+        '「芝犬」を「柴犬」に改める。',
+        '附 則',
+        'この法律は、別に法律で定める日から施行する。',
+        '理 由',
+        '誤字を修正するため。'
+    ]
+
+    bill_text = build_bill_text('Bill:1', texts)
+    assert bill_text.id == 'Bill:1'
+    assert bill_text.body == '犬法の一部を次のように改正する。「芝犬」を「柴犬」に改める。'
+    assert bill_text.supplement == 'この法律は、別に法律で定める日から施行する。'
+    assert bill_text.reason == '誤字を修正するため。'
+
+
+def test_build_bill_text_fail():
+    texts = [
+        'ワンワンワン'
+    ]
+    with pytest.raises(ValueError):
+        build_bill_text('Bill:1', texts)
+
+
+def test_get_offset():
+    assert get_offset('') == -1
+    assert get_offset('   ') == -1
+    assert get_offset('   こんにちは') == 3
